2b678ab91a184dd53eae91229dfa356e1cf17844
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-12-15 07:46:25 +0300

    add serializers

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Habit
+from .serializers import HabitSerializer
 
-# Create your views here.
+
+class HabitViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+    def get_queryset(self):
+        # Ограничиваем доступ только к привычкам текущего пользователя
+        return Habit.objects.filter(user=self.request.user)
